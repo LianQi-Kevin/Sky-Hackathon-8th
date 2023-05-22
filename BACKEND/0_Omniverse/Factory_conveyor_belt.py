a5fbeecd9702cded863274abbf8099e1199597e0
LianQi-Kevin <18201584863@163.com>
2023-05-22 22:48:16 +0800

    * add Factory_conveyor_belt.py v0.1 * add export preview tool (draw_kitti_label.py) * add requirements.txt (not final)

---
+++ BACKEND/0_Omniverse/Factory_conveyor_belt.py
@@ -0,0 +1,124 @@
+"""
+对 example.py 做一些修改以实现更多的物体变动和镜头移动，用于Sky-Hackathon-8th
+
+此处补充一些omni.replicator.core的注意事项
+1. rep.distribution.uniform(lower, upper)
+    lower 的每一个值都必须要小于 upper
+"""
+
+import omni.replicator.core as rep
+import os
+
+# setup random view range for camera: low point, high point
+sequential_pos = [(-800, 220, -271), (800, 220, 500)]
+
+# position of look-at target
+look_at_position = [(-430, 78, 10), (355, 78, 90)]
+
+# setup working layer
+with rep.new_layer():
+    # 无头模式下场景无默认光源，添加额外光源(参照默认环境光设置)
+    rep.create.light(light_type="Distant", temperature=6500, intensity=3000,
+                     rotation=(315, 0, 0), scale=1, name="DefaultLight")
+    # define 3d models: usd format file source link, class, initial position
+    WORKSHOP = 'http://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/ArchVis/Industrial/Buildings/Warehouse/Warehouse01.usd'
+    CONVEYOR = 'http://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/DigitalTwin/Assets/Warehouse/Equipment/Conveyors/ConveyorBelt_A/ConveyorBelt_A23_PR_NVD_01.usd'
+    BOX1 = 'http://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/ArchVis/Industrial/Containers/Cardboard/Cardbox_A3.usd'
+    BOX2 = 'http://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/ArchVis/Industrial/Containers/Cardboard/Cardbox_B3.usd'
+    BOX3 = 'http://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/ArchVis/Industrial/Containers/Cardboard/Cardbox_C3.usd'
+    BOX4 = 'http://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/ArchVis/Industrial/Containers/Cardboard/Cardbox_D3.usd'
+    workshop = rep.create.from_usd(WORKSHOP)
+    conveyor1 = rep.create.from_usd(CONVEYOR)
+    conveyor2 = rep.create.from_usd(CONVEYOR)
+    box1 = rep.create.from_usd(BOX1, semantics=[('class', 'box')])
+    box2 = rep.create.from_usd(BOX2, semantics=[('class', 'box')])
+    box3 = rep.create.from_usd(BOX3, semantics=[('class', 'box')])
+    box4 = rep.create.from_usd(BOX4, semantics=[('class', 'box')])
+
+    with workshop:
+        rep.modify.pose(
+            position=(0, 0, 0),
+            rotation=(0, -90, -90)
+        )
+    with conveyor1:
+        rep.modify.pose(
+            position=(-40, 0, 0),
+            rotation=(0, -90, -90)
+        )
+    with conveyor2:
+        rep.modify.pose(
+            position=(-40, 0, 100),
+            rotation=(-90, 90, 0)
+        )
+
+    with box1:
+        rep.modify.pose(
+            position=(-350, 78, 57),
+            rotation=(0, -90, -90),
+        )
+    with box2:
+        rep.modify.pose(
+            position=(-100, 78, 57),
+            rotation=(0, -90, -90),
+        )
+    with box3:
+        rep.modify.pose(
+            position=(100, 78, 57),
+            rotation=(0, -90, -90),
+        )
+    with box4:
+        rep.modify.pose(
+            position=(200, 78, 57),
+            rotation=(0, -90, -90),
+        )
+
+    # define lighting function
+    def sphere_lights(num):
+        lights = rep.create.light(
+            light_type="Sphere",
+            temperature=rep.distribution.normal(3500, 500),
+            intensity=rep.distribution.normal(15000, 5000),
+            position=rep.distribution.uniform((-300, -300, -300), (300, 300, 300)),
+            scale=rep.distribution.uniform(50, 100),
+            count=num
+        )
+        return lights.node
+
+    rep.randomizer.register(sphere_lights)
+
+    # define function to create random position range for target
+    def get_shapes():
+        # 选定所有("class", "box")的对象
+        shapes = rep.get.prims(semantics=[('class', 'box')])
+        # 针对选定的所有对象批量配置
+        with shapes:
+            rep.modify.pose(
+                position=rep.distribution.uniform((0, -50, 0), (0, 50, 0)),
+                scale=rep.distribution.uniform(0.5, 1.2)
+            )
+        return shapes.node
+
+    rep.randomizer.register(get_shapes)
+
+    # 创建相机并配置渲染
+    camera = rep.create.camera(position=sequential_pos[0], look_at=look_at_position[0])
+    render_product = rep.create.render_product(camera, resolution=(1024, 1024))
+
+    with rep.trigger.on_frame(num_frames=50):  # number of picture
+        rep.randomizer.sphere_lights(4)  # number of lighting source
+        rep.randomizer.get_shapes()
+        with camera:
+            rep.modify.pose(
+                position=rep.distribution.uniform(sequential_pos[0], sequential_pos[1]),
+                look_at=rep.distribution.uniform(look_at_position[0], look_at_position[1]))
+
+    # Initialize and attach writer for Kitti format data
+    writer = rep.WriterRegistry.get("KittiWriter")
+    writer.initialize(
+        output_dir=os.path.join(os.getcwd(), "_output"),
+        bbox_height_threshold=10,
+        fully_visible_threshold=0.95,
+        omit_semantic_type=True
+    )
+    writer.attach([render_product])
+    rep.orchestrator.preview()
---
+++ BACKEND/0_Omniverse/tools/draw_kitti_label.py
@@ -0,0 +1,22 @@
+from PIL import Image, ImageDraw
+import os
+import glob
+from tqdm import tqdm
+
+
+def main(img_path: str, label_path: str, output_path: str):
+    os.makedirs(output_path, exist_ok=True)
+    for img_path in tqdm(glob.glob(os.path.join(img_path, "*.png"))):
+        id_, _ = os.path.splitext(os.path.basename(img_path))
+        img = Image.open(img_path)
+        draw = ImageDraw.Draw(img)
+        with open(os.path.join(label_path, f"{id_}.txt")) as label_f:
+            for line in label_f.readlines():
+                line_list = line.split(" ")
+                x1, y1, x2, y2 = line_list[4], line_list[5], line_list[6], line_list[7],
+                draw.rectangle(xy=(float(x1), float(y1), float(x2), float(y2)), outline="red", width=3)
+        img.save(os.path.join(output_path, f"{id_}.png"))
+
+
+if __name__ == '__main__':
+    main(img_path="../_output/Camera/rgb", label_path="../_output/Camera/object_detection", output_path="_output")
